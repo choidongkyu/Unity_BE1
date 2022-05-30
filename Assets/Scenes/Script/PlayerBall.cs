20e1920bc55ba9efec741599ee260f5756dba1b3
dkchoi <ehdqkwk92@gmail.com>
2022-05-30 16:31:38 +0900

    player, Item 추가/Floor asset 추가

--- Assets/Scenes/Script/PlayerBall.cs
+++ Assets/Scenes/Script/PlayerBall.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class PlayerBall : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    void Start()
+    new Rigidbody rigidbody;
+    float jumpPower = 30;
+    private bool isJump;
+
+    void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        isJump = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigidbody.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        rigidbody.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            isJump = false;
+        }
     }
 }
